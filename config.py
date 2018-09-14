f666bf308216d522d58cace41dec00eaf3324760
Shomi Nanwani <11387975+0p71mu5@users.noreply.github.com>
2018-09-15 02:25:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ Created on Fri Dec  8 02:30:14 2017
 
 path_windows_state_alert = r'windows_state_info.xlsx'
 
-registered_mobile_windows_state_alert = ['9999999999','8888888888','7777777777']
-receiver_number_windows_state_alert = ['9999999999']
-pass_windows_state_alert=['password','password']
+registered_mobile_windows_state_alert = ['9999999999','8888888888','7777777777']  # numbers registered on way2sms
+receiver_number_windows_state_alert = ['9999999999']	                            # number where alert is to be sent
+pass_windows_state_alert=['password1','password2','password2']	                  # credentials for numbers registered in way2sms
 
 address = "www.google.com"
